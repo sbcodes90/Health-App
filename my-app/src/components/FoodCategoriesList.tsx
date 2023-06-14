3c97e9ef107aa9cfa440ab2ed8d667ae77f752cc
Sydney B <sydneycburrell29@gmail.com>
2023-06-14 21:52:09 +0000

    loader and list

--- my-app/src/components/FoodCategoriesList.tsx
+++ my-app/src/components/FoodCategoriesList.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { useLoaderData, LoaderFunction } from "react-router-dom";
+import { getCategories } from '../services/Categories';
+import CategoryOption from './CategoryOption';
 
 export const FoodCategoriesList = () => {
+    const data = useLoaderData() as Category[];
+
   return (
-    <div>FoodCategoriesList</div>
+    <div>FoodCategoriesList
+    
+    {data.map((item)=> {
+        return (
+            <div key={item.idCategory}>
+                <CategoryOption description={item.strCategoryDescription} image={item.strCategoryThumb} title={item.strCategory} />
+            </div>
+        )
+        
+    })}
+
+</div>
   )
 }
+
+export const foodCategoryListLoader: LoaderFunction = async () => {
+    const results = await getCategories();
+    return results;
+};
