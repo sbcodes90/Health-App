4d30622e89cb975dc564a013cc5561bd77cb4b8c
Sydney B <sydneycburrell29@gmail.com>
2023-06-15 01:40:16 +0000

    fixed grid

--- my-app/src/components/FoodCategoriesList.tsx
+++ my-app/src/components/FoodCategoriesList.tsx
@@ -9,14 +9,24 @@ export const FoodCategoriesList = () => {
   return (
     <div>FoodCategoriesList
     
-    {data.map((item)=> {
+    <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 p-5 gap-6">
+     {data.map((item) => {
+     return <div key={item.idCategory}>{item.strCategory}
+     <img src={item.strCategoryThumb} alt="" /></div>
+    })}
+     
+    </div>
+
+
+
+    {/* {data.map((item)=> {
         return (
             <div key={item.idCategory}>
                 <CategoryOption description={item.strCategoryDescription} image={item.strCategoryThumb} title={item.strCategory} />
             </div>
         )
         
-    })}
+    })} */}
 
 </div>
   )
