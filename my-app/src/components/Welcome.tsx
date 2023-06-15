fa040cb0882624d568ab860ebd6b18c922dd8e20
Sydney B <sydneycburrell29@gmail.com>
2023-06-15 01:40:06 +0000

    welcome screen & images

--- my-app/src/components/Welcome.tsx
+++ my-app/src/components/Welcome.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import logo from '../images/newlogo.png'
+import logoBackground from '../images/logobackground.png'
+import { Link } from 'react-router-dom';
 
 export const Welcome = () => {
+
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-    <p className="text-3xl text-gray-700 font-bold mb-5">
-      Welcome!
-    </p>
-    <p className="text-gray-500 text-lg">
-      React and Tailwind CSS in action
-    </p>
-  </div>
-  )
+    <>
+      <div className="flex justify-center">
+        <img src={logoBackground} alt={"logo"} />
+      </div>
+      <div className="h-screen bg-[#f97316] text-white">
+        <h1 className="text-4xl font-extrabold text-center p-20">
+          Welcome Back
+        </h1>
+        <div id="button-container" className="flex justify-around">
+        <button className="bg-white text-[#f97316] px-20 py-5 rounded-full font-bold">Spice it Up</button>
+        <button className="bg-white text-[#f97316] px-10 py-5 rounded-full font-bold"><Link to={'/options'}>Go to Food Categories</Link></button>
+        </div>
+      </div>
+ 
+    </>
+  );
 }
 
