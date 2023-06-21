a9383cc7a8bc3ee9516c1108bd02046d25daf452
Sydney B <sydneycburrell29@gmail.com>
2023-06-21 17:51:41 +0000

    getRandomMeal function

--- my-app/src/services/Categories.ts
+++ my-app/src/services/Categories.ts
@@ -4,7 +4,15 @@ export async function getCategories() {
     const response =  await axios.get(
         "https://www.themealdb.com/api/json/v1/1/categories.php"
       );
-      console.log('response from getCategories service',response.data.categories);
+      //console.log('response from getCategories service',response.data.categories);
       return response.data.categories;
      
+}
+
+export async function getRandomMeal() {
+  const response = await axios.get(
+    'https://www.themealdb.com/api/json/v1/1/random.php'
+  );
+  //console.log('from function',response.data.meals);
+  return response.data.meals;
 }
