924c6ec65a7e3edceec15d99c41df4a295ee893b
Sydney B <sydneycburrell29@gmail.com>
2023-06-21 17:50:32 +0000

    added new types

--- my-app/src/global.d.ts
+++ my-app/src/global.d.ts
@@ -4,3 +4,18 @@ interface Category {
     strCategoryThumb: string;
     strCategoryDescription: string
   }
+
+  interface RandomMealType {
+
+    idMeal: string;
+    strArea: string;
+    strMealThumb: string;
+    strMeal: string;
+  
+  }
+
+  interface Meals {
+    idMeal: string;
+    strMeal: string;
+    strMealThumb: string
+  }
