87da9b5577fcec2029d06dd837b8f4b53908b4a9
Sydney B <sydneycburrell29@gmail.com>
2023-06-23 22:40:36 +0000

    create social media section component

---
+++ my-app/src/components/SocialMediaSection.tsx
@@ -0,0 +1,26 @@
+export const SocialMediaSection = () => {
+
+    const randomLikes = Math.floor(Math.random() * 1000);
+    const randomComments = Math.floor(Math.random() * 100);
+
+    return (
+        <div className="flex space-x-2 justify-center pt-5">
+            <div className="flex space-x-1 items-center">
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 text-gray-600 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+                    </svg>
+                </span>
+                <span>{randomComments}</span>
+            </div>
+            <div className="flex space-x-1 items-center">
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 text-red-500 hover:text-red-400 transition duration-100 cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
+                    </svg>
+                </span>
+                <span>{randomLikes}</span>
+            </div>
+        </div>
+    )
+}
