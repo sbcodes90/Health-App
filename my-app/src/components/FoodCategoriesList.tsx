e3f15906625936606def1d6677238c7a2c3cf4f5
Sydney B <sydneycburrell29@gmail.com>
2023-06-23 20:31:53 +0000

    nav and css updates

--- my-app/src/components/FoodCategoriesList.tsx
+++ my-app/src/components/FoodCategoriesList.tsx
@@ -10,49 +10,18 @@ export const FoodCategoriesList = () => {
   return (
     <>
       <Nav />
-
- 
-      <div className='w-full h-auto p-20 bg-gradient-to-b from-orange-200 to-orange-400'>
-      <div className='text-black font-bold text-center pb-10 text-2xl'>Food Categories</div>
-      
+      <div className='w-full h-auto p-20 bg-black'>
+      <div className='text-white font-bold text-center pb-10 text-2xl'>Food Categories</div>
           <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center gap-10'>
-            {data.map((item) => (
-              <div key={item.idCategory} className="text-center p-10 rounded-xl bg-gray-50 bg-opacity-50 border-slate-100 border-4 transition ease-in-out delay-150 bg-white-100 hover:-translate-y-1 hover:scale-110 hover:bg-white-100 duration-300 ...">
-                <div><img src={item.strCategoryThumb} alt="cateogry-image" /></div>
+            {data.map((item) => (  
+              <div key={item.idCategory} className="text-center p-10 rounded-xl  bg-gradient-to-r from-orange-500 to-orange-800 bg-opacity-10 border-slate-100 border-4 transition ease-in-out delay-150 bg-white-100 hover:-translate-y-1 hover:scale-110 hover:bg-white-100 duration-300 ...">
+                <div><Link to={`option/${item.strCategory}`}><img src={item.strCategoryThumb} alt="cateogry-image" /></Link></div>
                 <br></br>
-                <div className="font-bold">{item.strCategory}</div>
+                <Link to={`option/${item.strCategory}`} className='justify-center font-bold'>{item.strCategory}</Link>
               </div>
             ))}
           </div>
         </div>
-      
-
-      {/* <div className="flex justify-center p-20 text-orange-500 font-bold text-3xl bg-black">
-        Food Categories List
-      </div>
-      <div className='bg-red p-20'>
-      <div className="grid p-20 grid-cols-1 gap-20 md:grid-cols-3 lg:grid-cols-4">
-      {data.map((item) => {
-          return (
-            <div key={item.idCategory} className='justify-center'>
-              <img src={item.strCategoryThumb} alt="food-category" className="mb-10 " />
-              <Link to={`option/${item.strCategory}`} className='justify-center font-bold'>{item.strCategory}</Link>
-            </div>
-          )
-        })}
-      </div>
-      {/* <div className="grid p-20 grid-cols-1 gap-20 md:grid-cols-3 lg:grid-cols-4">
-        {data.map((item) => {
-          return (
-            <div key={item.idCategory} className='justify-center'>
-              <img src={item.strCategoryThumb} alt="food-category" className="mb-10 " />
-              <Link to={`option/${item.strCategory}`} className='justify-center font-bold'>{item.strCategory}</Link>
-            </div>
-          )
-        })}
-
-      </div> 
-      </div> */}
     </>
   )
 }
