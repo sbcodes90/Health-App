ed12edb76b45dea6c0d917a266262a5caf5c3edd
Sydney B <sydneycburrell29@gmail.com>
2023-06-23 19:17:10 +0000

    food categories css

--- my-app/src/components/FoodCategoriesList.tsx
+++ my-app/src/components/FoodCategoriesList.tsx
@@ -10,17 +10,21 @@ export const FoodCategoriesList = () => {
   return (
     <>
       <Nav />
-      <div className='bg-black w-full h-[100vh] p-20'>
-        <div className=' grid grid-row-1 justify-center p-20 bg-white h-[50vh] pt-20 rounded '>
-          <div className='text-orange-500 font-bold'>Food Categories</div>
-          
-          {data.map((item) => (
-            <div key={item.idCategory}>{item.strCategory}</div>
-          ))}
+      <div className='bg-black w-full h-auto p-20'>
+        <div className="p-20 bg-white h-auto pt-20 rounded">
+          <div className='text-orange-500 font-bold text-center pb-10 text-2xl'>Food Categories</div>
+          <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center gap-10 '>
+            {data.map((item) => (
+              <div key={item.idCategory} className="text-center p-10 border-4 rounded-xl border-black">
+                <div><img src={item.strCategoryThumb} alt="cateogry-image" /></div>
+                <br></br>
+                <div className="font-bold">{item.strCategory}</div>
+              </div>
+            ))}
           </div>
-          
         </div>
-      
+      </div>
+
       {/* <div className="flex justify-center p-20 text-orange-500 font-bold text-3xl bg-black">
         Food Categories List
       </div>
