17bd2bf643d63048a479aca684a21e547542fb82
sbcodes90 <sydneycburrell29@gmail.com>
2024-01-02 19:45:58 -0500

    modal css update

--- my-app/src/components/Welcome.tsx
+++ my-app/src/components/Welcome.tsx
@@ -16,12 +16,12 @@ export const Modal = ({ open, onClose }: { open: boolean, onClose: () => void })
 
   return (
     <div className="fixed inset-0 bg-gray opacity-98 backdrop-blur-xl flex justify-center items-center p-20">
-      <div className="bg-white rounded-lg	 p-20 border-8 border-orange-400">
+      <div className="bg-gray  opacity-90 rounded-lg	 p-20 border-8 border-orange-400">
 
-           <div className="text-center pb-10 font-bold text-xl">{data && data[0]?.strMeal}</div>
-          <img src={data[0]?.strMealThumb} alt=""  className="h-[20px] lg:h-[40vh]" /> 
+           <div className="text-center pb-10 font-bold text-xl text-white">{data && data[0]?.strMeal}</div>
+          <img src={data[0]?.strMealThumb} alt=""  className="h-[40vh] lg:h-[40vh]" /> 
         <div className=" flex items-center justify-around gap-10 pt-10">
-        <Link className=" border-orange-400 border-2 rounded-lg	 hover:bg-white text-orange-500 font-bold py-1.5 px-8" to={`categories/${data[0]?.strCategory}/${data[0]?.idMeal}`}>Details</Link>
+        <Link className=" border-orange-400 border-2 rounded-lg	 hover:bg-black text-orange-500 font-bold py-1.5 px-8" to={`categories/${data[0]?.strCategory}/${data[0]?.idMeal}`}>Details</Link>
         <button
           onClick={onClose}
           className="bg-orange-500 hover:bg-orange-800 text-white font-bold py-2 px-10 rounded">
