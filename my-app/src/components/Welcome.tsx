2ee52b6d3efa3e362c806f2ebf3805b296863cca
sbcodes90 <sydneycburrell29@gmail.com>
2024-01-02 19:20:31 -0500

    css buttons/links update

--- my-app/src/components/Welcome.tsx
+++ my-app/src/components/Welcome.tsx
@@ -7,7 +7,6 @@ import food from '../images/food.jpg'
 export const Modal = ({ open, onClose }: { open: boolean, onClose: () => void }) => {
 
   const data = useLoaderData() as RandomMealType[];
-  console.log('data', data)
 
   if (!open) {
 
@@ -17,14 +16,15 @@ export const Modal = ({ open, onClose }: { open: boolean, onClose: () => void })
 
   return (
     <div className="fixed inset-0 bg-gray opacity-98 backdrop-blur-xl flex justify-center items-center p-20">
-      <div className="bg-white rounded p-20">
+      <div className="bg-white rounded-lg	 p-20 border-8 border-orange-400">
 
            <div className="text-center pb-10 font-bold text-xl">{data && data[0]?.strMeal}</div>
           <img src={data[0]?.strMealThumb} alt=""  className="lg:h-[40vh]" /> 
-        <div className="flex justify-center gap-10 pt-10">
-        <Link to={`categories/${data[0]?.strCategory}/${data[0]?.idMeal}`}>Details</Link>        <button
+        <div className=" flex items-center justify-around gap-10 pt-10">
+        <Link className=" border-orange-400 border-2 rounded-lg	 hover:bg-white text-orange-500 font-bold py-1.5 px-8" to={`categories/${data[0]?.strCategory}/${data[0]?.idMeal}`}>Details</Link>
+        <button
           onClick={onClose}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded r">
+          className="bg-orange-500 hover:bg-orange-800 text-white font-bold py-2 px-10 rounded">
           Close
         </button>
         </div>
@@ -44,10 +44,9 @@ export const Welcome = () => {
       <img src={logoBackground} alt="brand-logo" className="h-auto max-w-full" />
       <img src={food} alt="food" className="h-[60vh] w-full object-cover"/>
       <div className="w-full p-20 bg-black">
-        <div className="flex justify-center pt-10 lg:pb-20 text-4xl font-bold text-white">Let's Eat!</div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10 pb-20 m-20">
-          <div className="flex justify-center text-sm bg-orange-500 rounded-3xl p-6 text-black font-bold" onClick={() => setShowModal(true)}>Surprise Me!</div>
-          <div className="flex justify-center text-sm bg-orange-500 rounded-3xl p-6 text-black font-bold"><Link to={'/categories'}>View Categories</Link></div>
+          <div className="flex justify-center text-lg bg-orange-500 rounded-3xl p-6 text-black font-bold  border-orange-300 border-8" onClick={() => setShowModal(true)}>Surprise Me!</div>
+          <div className="flex justify-center text-lg bg-orange-500 rounded-3xl p-6 text-black font-bold border-orange-300 border-8"><Link to={'/categories'}>View Categories</Link></div>
         </div>
       </div>
 
